bd3aecd2ea4f9b496022c0268b911e6ec8db0e17
Belissimo-T <37810842+Belissimo-T@users.noreply.github.com>
2024-05-25 00:04:19 +0200

    asd

--- server.py
+++ server.py
@@ -1,17 +1,46 @@
-from flask import Flask
+import openai
+from flask import Flask, request
+
+import session
 
 app = Flask(__name__)
+
+SESSIONS: dict[str, session.Session] = {}
 
 
 @app.route("/create_session")
 def create_session():
-    return "Hello, World!"
+    i = 0
+    while f"session_{i}" in SESSIONS:
+        i += 1
+
+    location_determiner = session.LocationDeterminer()
+    dialog_agent = session.OpenAiDialogAgent(
+        openai_client=openai.Client(),
+    )
+    SESSIONS[f"session_{i}"] = session.Session(location_determiner, dialog_agent)
+
+    return {"session_id": f"session_{i}"}
+
+
+def error(message: str):
+    return {"error": message}
 
 
 @app.route("/get_response")
 def get_response():
-    return "Hello, World!"
+    if "session_id" not in request.args:
+        return error("session_id is required")
 
+    if "prompt" not in request.args:
+        return error("prompt is required")
+
+    session_id = request.args["session_id"]
+
+    if session_id not in SESSIONS:
+        return error("Invalid session_id")
+
+    return {"response": SESSIONS[session_id].get_response(request.args["prompt"])}
 
 
 if __name__ == '__main__':
--- session.py
+++ session.py
@@ -2,6 +2,7 @@ import abc
 import dataclasses
 import json
 import typing
+import openai
 
 
 class LocationDeterminer(abc.ABC):
@@ -18,6 +19,37 @@ class DialogAgent(abc.ABC):
     @abc.abstractmethod
     def get_response(self) -> str:
         ...
+
+
+@dataclasses.dataclass
+class Message:
+    role: str
+    content: str
+
+
+@dataclasses.dataclass
+class OpenAiDialogAgent(DialogAgent):
+    openai_client:  openai.Client
+    model: str = "gpt-4o"
+
+    messages: list[Message] = dataclasses.field(default_factory=list)
+
+    def add_message(self, message: str, role: str):
+        self.messages.append(Message(role, message))
+
+    def get_response(self) -> str:
+        messages_serialized = [
+            {"role": message.role, "content": message.content}
+            for message in self.messages
+        ]
+
+        # noinspection PyTypeChecker
+        response = self.openai_client.chat.completions.create(
+            model=self.model,
+            messages=messages_serialized
+        )
+
+        breakpoint()
 
 
 JSON_KEYWORD = "CALL_DATA_AGENT"

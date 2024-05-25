4cc084ef2493ed7442fe1df5b577244bc2b76441
lorax <142108266+lorax42@users.noreply.github.com>
2024-05-25 19:33:22 +0200

    add better exception catching

--- logic-server/src/klips_json.py
+++ logic-server/src/klips_json.py
@@ -31,8 +31,8 @@ def get_weather(ids: int, date: str):
     try:
         t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
         t_2 = datetime.datetime.strftime(date + datetime.timedelta(hours=2), date_fmt)
-    except Exception:
-        print("WARNING: trouble converting date to string")
+    except Exception as e:
+        raise Exception("WARNING: trouble converting date to string") from e
 
     base_url = 'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items'
     time_filter = f'?filter=messzeit>"{t_1}" and messzeit<"{t_2}"'
@@ -45,12 +45,12 @@ def get_weather(ids: int, date: str):
 
     try:
         req = requests.get(temp_url)  # request data
-    except Exception:
-        print(f"WARNING: trouble requesting data from {temp_url}")
+    except Exception as e:
+        raise Exception(f"WARNING: trouble requesting data from {temp_url}") from e
     try:
         req_json = json.loads(req.text)  # parse json
-    except Exception:
-        print("WARNING: trouble converting requested text to JSON")
+    except Exception as e:
+        raise Exception("WARNING: trouble converting requested text to JSON") from e
 
     return req_json
 
@@ -58,20 +58,20 @@ def get_weather(ids: int, date: str):
 def request(coord: tuple[float, float], str_date: str):
     try:
         nearest_id = get_nearest_id(coord)
-    except Exception:
-        print("WARNING: trouble in nearest_id()")
+    except Exception as e:
+        raise Exception("WARNING: trouble in nearest_id()") from e
 
     print(f"nearestID: {nearest_id}")
 
     try:
         weather_json = get_weather(nearest_id, str_date)
-    except Exception:
-        print("WARNING: trouble in get_weather()")
+    except Exception as e:
+        raise Exception("WARNING: trouble in get_weather()") from e
 
     try:
         weather_list = [(entry['properties']['messzeit'], entry['properties']['wert_n']) for entry in weather_json['features']]
-    except Exception:
-        print("WARNING: trouble writing JSON to list")
+    except Exception as e:
+        raise Exception("WARNING: trouble writing JSON to list") from e
     return weather_list
 
 
@@ -81,25 +81,25 @@ coord = [13.7312841000004, 51.0442978999856]
 
 try:
     date = datetime.datetime.strptime(date, date_fmt)
-except Exception:
-    print("WARNING: trouble converting string to date")
+except Exception as e:
+    raise Exception("WARNING: trouble converting string to date") from e
 try:
     t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
     t_2 = datetime.datetime.strftime(date + datetime.timedelta(hours=2), date_fmt)
-except Exception:
-    print("WARNING: trouble converting date to string")
+except Exception as e:
+    raise Exception("WARNING: trouble converting date to string") from e
 
 id_url = f'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items?filter=messzeit>"{t_1}" and messzeit<"{t_2}"&propertynames=geom,ids'
 
 try:
     req = requests.get(id_url)  # request data
-except Exception:
-    print(f"WARNING: trouble requesting data from {id_url}")
+except Exception as e:
+    raise Exception(f"WARNING: trouble requesting data from {id_url}") from e
 
 try:
     req_json = json.loads(req.text)  # parse json
-except Exception:
-    print("WARNING: trouble converting requested text to JSON")
+except Exception as e:
+    raise Exception("WARNING: trouble converting requested text to JSON") from e
 
 IDS = {}
 
@@ -109,7 +109,7 @@ for feature in req_json['features']:
 
     try:
         IDS[feature['id']] = feature['geometry']['coordinates']
-    except Exception:
-        print("WARNING: trouble retrieving id")
+    except Exception as e:
+        raise Exception("WARNING: trouble retrieving id") from e
 
 print(f"number of distinct ids: {len(IDS)}")

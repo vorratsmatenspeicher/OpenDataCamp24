cf1c09dce59332fe3eac6bbe2a07b2472cc7f0bb
lorax <142108266+lorax42@users.noreply.github.com>
2024-05-25 17:43:49 +0200

    clean up klips_json.py

--- logic-server/src/klips_json.py
+++ logic-server/src/klips_json.py
@@ -6,9 +6,6 @@ import json
 import datetime
 from urllib import parse
 
-
-# url = 'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items?filter=messzeit>"2024-05-01 06:00:00" and messzeit<"2024-05-02 06:00:00" and parameter~~"Lufttemperatur"&propertynames=geom,ids,messzeit,wert_n,einheit,bezeichnung,parameter'
-
 def get_nearest_id(coord: tuple[float, float]) -> int:
     def dist_to(id: int) -> float:
         a, b = coord
@@ -18,7 +15,6 @@ def get_nearest_id(coord: tuple[float, float]) -> int:
 
     return min(IDS, key=dist_to)
 
-
 def get_weather(ids: int, date: str):
     date = datetime.datetime.strptime(date, date_fmt)
     t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
@@ -27,19 +23,14 @@ def get_weather(ids: int, date: str):
     base_url = 'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items'
     time_filter = f'?filter=messzeit>"{t_1}" and messzeit<"{t_2}"'
     ids_filter = f' and ids=={ids}'
-    # coord_filter = '&propertynames=geom,ids'
     temp_filter = 'and parameter~~"Lufttemperatur"&propertynames=ids,parameter,wert_n,messzeit'
 
-    # coord_url = base_url + time_filter + ids_filter + coord_filter
     temp_url = base_url + time_filter + ids_filter + temp_filter
 
-    # print(f"coord_url: {coord_url}")
     print(f"temp_url: {temp_url}")
 
     req = requests.get(temp_url)  # request data
     req_json = json.loads(req.text)  # parse json
-
-    # print(f"keys: {list(req_json.keys())}") # show keys
 
     return req_json
 
@@ -51,7 +42,6 @@ def request(coord: tuple[float, float], str_date: str):
 
     weather_list = [(entry['properties']['messzeit'], entry['properties']['wert_n']) for entry in
                     weather_json['features']]
-    # return weather_json
     return weather_list
 
 
@@ -63,7 +53,6 @@ date = datetime.datetime.strptime(str_date, date_fmt)
 t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
 t_2 = datetime.datetime.strftime(date + datetime.timedelta(hours=2), date_fmt)
 
-# id_url = 'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items?filter=messzeit%3E%222024-05-01%2004:00:00%22%20and%20messzeit%3C%222024-05-01%2008:00:00%22%20and%20ids==920&propertynames=ids,parameter,wert_n'
 id_url = f'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items?filter=messzeit>"{t_1}" and messzeit<"{t_2}"&propertynames=geom,ids'
 
 req = requests.get(id_url)  # request data
@@ -78,5 +67,3 @@ for feature in req_json['features']:
     IDS[feature['id']] = feature['geometry']['coordinates']
 
 print(f"number of distinct ids: {len(IDS)}")
-
-# print(request(coord, str_date))

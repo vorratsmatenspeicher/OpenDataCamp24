45ec4a6954c3396e31f4762bbc2db14831018d5c
Belissimo-T <37810842+Belissimo-T@users.noreply.github.com>
2024-05-25 11:23:12 +0200

    test chat

--- session.py
+++ session.py
@@ -1,14 +1,25 @@
+from __future__ import annotations
+
 import abc
 import dataclasses
+import datetime
 import json
+import textwrap
 import typing
+from pathlib import Path
+
 import openai
 
 
-class LocationDeterminer(abc.ABC):
-    @abc.abstractmethod
-    def determine_location(self, description: str):
-        ...
+class DataAgent:
+    def get_services_description(self) -> str:
+        return Path("services.json").read_text()
+
+    def invoke_service(self, service: str, arguments: dict) -> dict:
+        if service == "CLOCK":
+            return {"time": datetime.datetime.now().isoformat()}
+        else:
+            return {"error": f"Unknown service {service}"}
 
 
 class DialogAgent(abc.ABC):
@@ -29,12 +40,16 @@ class Message:
 
 @dataclasses.dataclass
 class OpenAiDialogAgent(DialogAgent):
-    openai_client:  openai.Client
-    model: str = "gpt-4o"
+    openai_client: openai.Client
+    model: str = "gpt-3.5-turbo"
 
     messages: list[Message] = dataclasses.field(default_factory=list)
 
     def add_message(self, message: str, role: str):
+        if role == "system":
+            print(f"SYS> {message}")
+        elif role == "user":
+            print(f"USR> {message}")
         self.messages.append(Message(role, message))
 
     def get_response(self) -> str:
@@ -49,24 +64,42 @@ class OpenAiDialogAgent(DialogAgent):
             messages=messages_serialized
         )
 
-        breakpoint()
+        msg = response.choices[0].message
+        self.add_message(msg.content, "system")
+
+        return msg.content
 
 
-JSON_KEYWORD = "CALL_DATA_AGENT"
+datetime.datetime.strptime("2024-05-01 06:00:00", "%Y-%m-%d %H:%M:%S") - datetime.timedelta(hours=2)
 
 
 @dataclasses.dataclass
 class Session:
-    location_determiner: LocationDeterminer
+    data_agent: DataAgent
     dialog_agent: DialogAgent
 
     def get_response(self, prompt: str) -> str:
         self.dialog_agent.add_message(prompt, "user")
-        response = self.dialog_agent.get_response()
+        response = self.dialog_agent.get_response().strip()
 
-        if response.startswith(JSON_KEYWORD):
-            data = json.loads(response[len(JSON_KEYWORD):])
-            location = self.location_determiner.determine_location(data["description"])
-            self.dialog_agent.add_message(f"Location: {location}", "system")
+        if "{" in response:
+            potential_json = response[response.index("{"):]
+            print("LLM responded with service call...", potential_json)
+            try:
+                parsed = json.loads(potential_json)
 
-        return self.dialog_agent.get_response()
+                service = parsed["service"]
+                args = parsed["args"]
+            except (KeyError, json.JSONDecodeError):
+                return f"Invalid service call {response}"
+            else:
+                result = self.data_agent.invoke_service(service, args)
+
+                self.dialog_agent.add_message(
+                    message=json.dumps(result, ensure_ascii=False),
+                    role="system"
+                )
+
+            return self.dialog_agent.get_response()
+        else:
+            return response

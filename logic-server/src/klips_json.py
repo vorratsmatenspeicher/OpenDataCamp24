53406ae8d5c6ab0a98189bc2357d5ac377f728c2
Belissimo-T <37810842+Belissimo-T@users.noreply.github.com>
2024-05-25 20:55:42 +0200

    fix klips and timezone stuff

--- logic-server/src/klips_json.py
+++ logic-server/src/klips_json.py
@@ -1,6 +1,5 @@
 # request weather data from klips
 # Zeit + Ort -> Wetter
-
 import requests
 import json
 import datetime
@@ -15,24 +14,19 @@ def get_nearest_id(coord: tuple[float, float]) -> int:
 
         dist = ((lat - lat_closest) ** 2 + (lon - lon_closest) ** 2)**0.5
 
-        if dist > distMargin:
-            raise ValueError("Closest data point is far away.")
+        # if dist > distMargin:
+        #     raise Exception("No accurate data available: closest sensor is far away.")
 
         return dist
-        
+
     return min(IDS, key=dist_to)
 
-def get_weather(ids: int, date: str):
-    try:
-        date = datetime.datetime.strptime(date, date_fmt)
-    except Exception as e:
-        raise Exception("WARNING: trouble converting string to date") from e
 
-    try:
-        t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
-        t_2 = datetime.datetime.strftime(date + datetime.timedelta(hours=2), date_fmt)
-    except Exception as e:
-        raise Exception("WARNING: trouble converting date to string") from e
+def get_weather(ids: int, date: str):
+    date = datetime.datetime.fromisoformat(date)
+
+    t_1 = datetime.datetime.strftime(date - datetime.timedelta(hours=2), date_fmt)
+    t_2 = datetime.datetime.strftime(date + datetime.timedelta(hours=2), date_fmt)
 
     base_url = 'https://kommisdd.dresden.de/net4/public/ogcapi/collections/L1625/items'
     time_filter = f'?filter=messzeit>"{t_1}" and messzeit<"{t_2}"'
@@ -43,35 +37,31 @@ def get_weather(ids: int, date: str):
 
     print(f"temp_url: {temp_url}")
 
-    try:
-        req = requests.get(temp_url)  # request data
-    except Exception as e:
-        raise Exception(f"WARNING: trouble requesting data from {temp_url}") from e
-    try:
-        req_json = json.loads(req.text)  # parse json
-    except Exception as e:
-        raise Exception("WARNING: trouble converting requested text to JSON") from e
+    req = requests.get(temp_url)  # request data
+    req_json = json.loads(req.text)  # parse json
 
     return req_json
 
 
 def request(coord: tuple[float, float], str_date: str):
-    try:
-        nearest_id = get_nearest_id(coord)
-    except Exception as e:
-        raise Exception("WARNING: trouble in nearest_id()") from e
+    nearest_id = get_nearest_id(coord)
 
     print(f"nearestID: {nearest_id}")
 
     try:
         weather_json = get_weather(nearest_id, str_date)
     except Exception as e:
-        raise Exception("WARNING: trouble in get_weather()") from e
+        e.add_note("WARNING: trouble in get_weather()")
+        raise
 
     try:
-        weather_list = [(entry['properties']['messzeit'], entry['properties']['wert_n']) for entry in weather_json['features']]
+        weather_list = [(
+            datetime.datetime.strptime(entry['properties']['messzeit'], "%d.%m.%Y %H:%M:%S"),
+            entry['properties']['wert_n']
+        ) for entry in weather_json['features']]
     except Exception as e:
-        raise Exception("WARNING: trouble writing JSON to list") from e
+        e.add_note("WARNING: trouble retrieving data from JSON")
+        raise
     return weather_list
 
 
@@ -80,7 +70,7 @@ str_date = "2024-05-01 06:00:00"
 coord = [13.7312841000004, 51.0442978999856]
 
 try:
-    date = datetime.datetime.strptime(date, date_fmt)
+    date = datetime.datetime.strptime(str_date, date_fmt)
 except Exception as e:
     raise Exception("WARNING: trouble converting string to date") from e
 try:
